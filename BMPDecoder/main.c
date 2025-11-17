05045517e3fdb77dc210afc7e5945034e6e6ae7a
Vedant Bhale <vedant.bhale@ymail.com>
2025-11-17 07:44:33 +0530

    Updated main.c

--- BMPDecoder/main.c
+++ BMPDecoder/main.c
@@ -128,7 +128,7 @@ void printImageScrn()
     uint32_t hor = image->dib_header.pixel_x;
     uint32_t ver = image->dib_header.pixel_y;
     uint32_t row_bytes = image->dib_header.raw_bitmap_size / image->dib_header.pixel_y;
-    printf("\n Byte num  = %d\n\n", row_bytes);
+
 
     for(int32_t row = ver; row > 0; row--)
     {
@@ -153,29 +153,30 @@ void printImageScrn()
     }
 }
 
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
 
     if( getImageArray(IMAGE_PATH_REL) )
     {
-        printf("\n Sizeof header = %d, size of dib header = %d", sizeof(T_BMP_HEADER), sizeof(T_DIB_HEADER));
-        printf("\n Magic bytes = %c%c", image->bmp_header.magic_bytes[0], image->bmp_header.magic_bytes[1]);
-        printf("\n File size = %d", image->bmp_header.file_size);
-        printf("\n Image array offset = %d", image->bmp_header.img_arr_offset);
-        printf("\n");
-        printf("\n DIB header size = %d", image->dib_header.dib_header_size);
-        printf("\n Pixel X = %d", image->dib_header.pixel_x);
-        printf("\n Pixel Y = %d", image->dib_header.pixel_y);
-        printf("\n Color plane number = %d", image->dib_header.color_plane_num);
-        printf("\n Bits per pixel = %d", image->dib_header.bits_per_pixel);
-        printf("\n Compression = %d", image->dib_header.compression);
-        printf("\n Raw bitmap size = %d", image->dib_header.raw_bitmap_size);
-        printf("\n Res X = %d", image->dib_header.img_res_x);
-        printf("\n Res Y = %d", image->dib_header.img_res_y);
-        printf("\n Color pallate number = %d", image->dib_header.color_pal_num);
-        printf("\n Important colors = %d", image->dib_header.imp_colors);
-        printf("\n\n");
+//        printf("\n Sizeof header = %d, size of dib header = %d", sizeof(T_BMP_HEADER), sizeof(T_DIB_HEADER));
+//        printf("\n Magic bytes = %c%c", image->bmp_header.magic_bytes[0], image->bmp_header.magic_bytes[1]);
+//        printf("\n File size = %d", image->bmp_header.file_size);
+//        printf("\n Image array offset = %d", image->bmp_header.img_arr_offset);
+//        printf("\n");
+//        printf("\n DIB header size = %d", image->dib_header.dib_header_size);
+//        printf("\n Pixel X = %d", image->dib_header.pixel_x);
+//        printf("\n Pixel Y = %d", image->dib_header.pixel_y);
+//        printf("\n Color plane number = %d", image->dib_header.color_plane_num);
+//        printf("\n Bits per pixel = %d", image->dib_header.bits_per_pixel);
+//        printf("\n Compression = %d", image->dib_header.compression);
+//        printf("\n Raw bitmap size = %d", image->dib_header.raw_bitmap_size);
+//        printf("\n Res X = %d", image->dib_header.img_res_x);
+//        printf("\n Res Y = %d", image->dib_header.img_res_y);
+//        printf("\n Color pallate number = %d", image->dib_header.color_pal_num);
+//        printf("\n Important colors = %d", image->dib_header.imp_colors);
+//        printf("\n\n");
 
         printImageScrn();
 
